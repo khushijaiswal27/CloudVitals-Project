055d8e78c3dc4e4ca63590dc6dd45610704a070a
Khushi Jaiswal <khushijaiswal.official27@gmail.com>
2026-04-10 20:09:01 +0530

    Day 3: Set up Flask structure and connected login page

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template  # <--- render_template ko yahan add karo
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return "<h1>CloudVitals Server is Running!</h1><p>Khushi, tumhara pehla backend successfully chal gaya!</p>"
+
+def home():
+    # Ab ye "index.html" ko templates folder mein dhundega
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
