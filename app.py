5b85bf7286dbd20f2bfd0f72152336793a1ddc81
Khushi Jaiswal <khushijaiswal.official27@gmail.com>
2026-04-11 20:12:58 +0530

    Day4: Linked homepage with Login page and fixed Flask routes

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
-from flask import Flask, render_template  # <--- render_template ko yahan add karo
+from flask import Flask, render_template # <--- render_template ko yahan add karo
 
 app = Flask(__name__)
 
+#ye tumhara homepage hai
 @app.route('/')
-
 def home():
-    # Ab ye "index.html" ko templates folder mein dhundega
-    return render_template('index.html')
+    # Ab ye "homepage.html" ko templates folder mein dhundega
+    return render_template('homepage.html')
+
+# Ye mera login page ka rasta hai
+@app.route('/login')
+def login_page():
+    # Ab ye "Login.html" ko templates folder mein dhundega
+    return render_template('Login.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
