94fc56f214ef12fee188e889548d0ba89d0d521d
Khushi Jaiswal <khushijaiswal.official27@gmail.com>
2026-06-17 20:21:23 +0530

    updated  DB file

--- app.py
+++ app.py
@@ -76,7 +76,8 @@ db = SQLAlchemy(app)
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True, nullable=False)
-    server_ip = db.Column(db.String(100), nullable=False)
+    # server_ip = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False) 
     password = db.Column(db.String(200), nullable=False)
     #email = db.Column(db.String(200), nullable=False)
 
