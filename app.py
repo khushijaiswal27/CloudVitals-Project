940fcc5e74d69ffbd7704234dc61ab536a9294be
Garima Choudhary <cgarima098@gmail.com>
2026-06-22 20:47:32 +0530

    updated the flask code

--- app.py
+++ app.py
@@ -76,9 +76,9 @@ db = SQLAlchemy(app)
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True, nullable=False)
-    server_ip = db.Column(db.String(100), nullable=False)
+    #server_ip = db.Column(db.String(100), nullable=False)
     password = db.Column(db.String(200), nullable=False)
-    #email = db.Column(db.String(200), nullable=False)
+    email = db.Column(db.String(200), nullable=False)
 
 # Create the database file automatically if it doesn't exist
 with app.app_context():
